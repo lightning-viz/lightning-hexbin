7a5f8fd7de3285b7f4f40eb3e10b59aeb011a608
Matthew Conlen <mc@mathisonian.com>
2015-06-22 01:03:55 -0400

    initial commit

---
+++ src/components/hexbin.js
@@ -0,0 +1,69 @@
+
+var hexbin = require('../../deps/hexbin');
+var Bin = require('./bin');
+var _ = require('lodash');
+var React = require('react');
+var utils = require('lightning-client-utils');
+
+
+var Hexbin = React.createClass({
+
+    getDefaultProps: function() {
+        return {
+          points: [],
+          initialRadius: 20
+        };
+    },
+
+    getInitialState: function() {
+        return {
+            radius: this.props.initialRadius
+        };
+    },
+
+    hexbin: function() {
+        return hexbin().size([this.props.width, this.props.height]).radius(this.state.radius);
+    },
+
+    componentDidUpdate: function() {
+        var context = this.getDOMNode().getContext('2d');
+        console.log('clearing rect');
+        context.clearRect(0, 0, this.props.width, this.props.height);
+        // this.forceUpdate();
+
+        console.log(this.props.childen);
+        this.props.childen.map(function(c) {
+            c.forceUpdate();
+        })
+    },
+
+    drawNodes: function() {
+        var hexbin = this.hexbin();
+        var path = hexbin.hexagon();
+
+        var data = hexbin(this.props.points);
+        var value = _.pluck(data, 'length');
+        var colors = utils.getColorFromData({
+            value: value
+        });
+
+        return _.map(data, function(d, i) {
+            return (<Bin path={path} x={d.x} y={d.y} getCanvasNode={this.getDOMNode} color={colors[i]} />)
+        }, this);
+    },
+
+    render: function() {
+        return (
+            <canvas 
+                width={this.props.width} 
+                height={this.props.height}>
+                {this.drawNodes()}
+            </canvas>
+        )
+    }
+ 
+});
+
+
+module.exports = Hexbin;
+
